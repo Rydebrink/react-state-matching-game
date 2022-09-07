66256c60caab59d8753b377321d067ab9f207112
Rydebrink <simon@rydebrink.se>
2022-09-07 12:05:44 +0200

    Module 3 working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,6 +18,44 @@ class App extends Component {
     };
   }
 
+  handleTileClicked = (id, color) => {
+    this.setState((state) => {
+      const tiles = state.tiles;
+      let toBeCleared = state.toBeCleared;
+      const selectedTileIndex = indexOfSelected(tiles, id, color);
+      let previousTileIndex = state.previousTileIndex;
+
+      if (toBeCleared !== null) {
+        tiles[toBeCleared[0]].selected = false;
+        tiles[toBeCleared[1]].selected = false;
+        toBeCleared = null;
+      }
+
+      tiles[selectedTileIndex].selected = true;
+
+      if (previousTileIndex !== null) {
+        const previousTile = tiles[previousTileIndex];
+        const selectedTile = tiles[selectedTileIndex];
+
+        if (
+          previousTile.id !== selectedTile.id &&
+          previousTile.color === color
+        ) {
+          selectedTile.matched = true;
+          previousTile.matched = true;
+          previousTileIndex = null;
+        } else {
+          toBeCleared = [previousTileIndex, selectedTileIndex];
+          previousTileIndex = null;
+        }
+      } else {
+        previousTileIndex = selectedTileIndex;
+      }
+
+      return { toBeCleared, tiles, previousTileIndex };
+    });
+  };
+
   startGame = (numTiles) => {
     this.setState((state) => ({
       playing: true,
@@ -26,39 +64,6 @@ class App extends Component {
       tiles: createTiles(state.numTiles, this.handleTileClicked),
     }));
   };
-
-  handleTileClicked(id, color) {
-    this.setState((state) => {
-      const tiles = state.tiles;
-      let toBeCleared = state.toBeCleared;
-      const selectedTileIndex = indexOfSelected(tiles, id, color);
-      let previousTileIndex = state.previousTileIndex;
-    });
-
-    if (toBeCleared !== null) {
-      tiles[toBeCleared[0]].selected = false;
-      tiles[toBeCleared[1]].selected = false;
-      toBeCleared = null;
-    }
-
-    tiles[selectedTileIndex].selected = true;
-
-    if (previousTileIndex !== null) {
-      const previousTile = tiles[previousTileIndex];
-      const selectedTile = tiles[selectedTileIndex];
-      if (previousTile.id !== selectedTile.id && previousTile.color === color) {
-        selectedTile.matched = true;
-        previousTile.matched = true;
-        previousTileIndex = null;
-      } else {
-        toBeCleared = [previousTileIndex, selectedTileIndex];
-        previousTileIndex = null;
-      }
-    } else {
-      previousTileIndex = selectedTileIndex;
-    }
-    return { tiles, toBeCleared, previousTileIndex };
-  }
 
   render() {
     return (
